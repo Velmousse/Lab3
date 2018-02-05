34e95eee7978ad6d3769aa5bec6cfe21489a48b9
Vincent <supravelmousse@gmail.com>
2018-02-05 13:49:37 -0500

    Version 1.00

--- src/main.java
+++ src/main.java
@@ -4,45 +4,16 @@
 import java.util.Scanner;
 
 public class main {
-    public static void main(String[] args) {
-        String position = "premier";
-        int entree = 0;
-        boolean fin = false;
+    private static Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Personnage j1 = null;
         Personnage j2 = null;
 
-        position = "premier";
-        System.out.print("Bienvenue dans RPG Simulator!\n\n");
-        for (int i = 0; i < 2; i++) {
-            System.out.print("Veuillez choisir le " + position + " personnage\n" +
-                    " 1- Barbare\n" +
-                    " 2- Paladin\n" +
-                    " 3- Magicien noir\n" +
-                    " 4- Magicien rouge\n" +
-                    "> ");
-            entree = sc.nextInt();
-            System.out.println();
-            switch (entree) {
-                case 1:
-                    if (position.equals("premier")) j1 = new Barbare();
-                    else j2 = new Barbare();
-                    break;
-                case 2:
-                    if (position.equals("premier")) j1 = new Paladin();
-                    else j2 = new Paladin();
-                    break;
-                case 3:
-                    if (position.equals("premier")) j1 = new MagicienNoir();
-                    else j2 = new MagicienNoir();
-                    break;
-                case 4:
-                    if (position.equals("premier")) j1 = new MagicienRouge();
-                    else j2 = new MagicienRouge();
-            }
-            position = "deuxième";
-        }
+        j1 = choixPersonnages("premier");
+        j2 = choixPersonnages("deuxieme");
+
         combat(j1, j2);
 
         sc.close();
@@ -63,5 +34,24 @@ public class main {
                 return;
             }
         }
+    }
+
+    public static Personnage choixPersonnages(String position) {
+        int entree = 0;
+        System.out.print("Veuillez choisir le " + position + " personnage\n" +
+                " 1- Barbare\n" +
+                " 2- Paladin\n" +
+                " 3- Magicien noir\n" +
+                " 4- Magicien rouge\n" +
+                "> ");
+        entree = sc.nextInt();
+        System.out.println();
+        switch (entree) {
+            case 1: return new Barbare();
+            case 2: return new Paladin();
+            case 3: return new MagicienNoir();
+            case 4: return new MagicienRouge();
+        }
+        return null;
     }
 }
